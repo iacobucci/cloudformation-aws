d22c458fe01326826c8e1a120a607a3842362fe9
iacobucci <iacobuccivalerio@gmail.com>
2023-05-02 16:51:58 +0200

    modified:   deploy.sh 	modified:   ecs.yml 	modified:   exported.txt 	modified:   lb.yml 	modified:   pipeline.yml

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 aws cloudformation create-stack --stack-name valerio-cfn-vpc --template-body file://vpc.yml
 aws cloudformation create-stack --stack-name valerio-cfn-lb --template-body file://lb.yml
-aws cloudformation create-stack --stack-name valerio-cfn-ecs --template-body file://ecs.yml --parameters file://parameters.json
+aws cloudformation create-stack --stack-name valerio-cfn-ecs --template-body file://ecs.yml
+aws cloudformation create-stack --stack-name valerio-cfn-pipeline --template-body file://pipeline.yml
